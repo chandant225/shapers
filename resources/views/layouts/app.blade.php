4cf53d01c76643d176d10cdb41ba6939c1f06b8c
devchandant225 <devchandant225@gmail.com>
2022-06-17 10:21:11 +0545

    swiper

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,7 +32,7 @@
     </div>
 
     <!-- Scroll-Top-btn -->
-    <a href="#" class="scroll-top btn-hover">
+    <a href="#" id="myBtn" class="scroll-top btn-hover">
         <i class="fas fa-arrow-up"></i>
     </a>
     @yield('script')
@@ -142,6 +142,30 @@
                 }
             }
         });
+    </script>
+
+    <script>
+        //Get the button
+        var mybutton = document.getElementById("myBtn");
+
+        // When the user scrolls down 20px from the top of the document, show the button
+        window.onscroll = function() {
+            scrollFunction()
+        };
+
+        function scrollFunction() {
+            if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+                mybutton.style.display = "block";
+            } else {
+                mybutton.style.display = "none";
+            }
+        }
+
+        // When the user clicks on the button, scroll to the top of the document
+        function topFunction() {
+            document.body.scrollTop = 0;
+            document.documentElement.scrollTop = 0;
+        }
     </script>
 </body>
 
