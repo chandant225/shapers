e0e2235ed44e4814520df68b1236132aa41ccfa0
devchandant225 <devchandant225@gmail.com>
2022-06-19 14:31:09 +0545

    project

--- resources/views/project.blade.php
+++ resources/views/project.blade.php
@@ -38,23 +38,17 @@
                             related projects to date.
                         </p>
                         <p class="my-2">
-                            <b
-                                >To learn about projects that fall under these
+                            <b>To learn about projects that fall under these
                                 impact areas, please click on the icons
-                                below.</b
-                            >
+                                below.</b>
                         </p>
 
                         <div class="impact-cards">
                             <a href="#">
                                 <div class="impact-card">
                                     <div class="impact-icon">
-                                        <img
-                                            src="{{
-                                                asset('assets/impact/1.png')
-                                            }}"
-                                            alt=""
-                                        />
+                                        <img src="{{ asset('assets/impact/1.png') }}"
+                                            alt="" />
                                     </div>
                                     <div class="impact-text">
                                         <h5>I for Iclusion</h5>
@@ -65,12 +59,8 @@
                             <a href="#">
                                 <div class="impact-card">
                                     <div class="impact-icon">
-                                        <img
-                                            src="{{
-                                                asset('assets/impact/2.png')
-                                            }}"
-                                            alt=""
-                                        />
+                                        <img src="{{ asset('assets/impact/2.png') }}"
+                                            alt="" />
                                     </div>
                                     <div class="impact-text">
                                         <h5>I for Iclusion</h5>
@@ -81,12 +71,8 @@
                             <a href="#">
                                 <div class="impact-card">
                                     <div class="impact-icon">
-                                        <img
-                                            src="{{
-                                                asset('assets/impact/3.png')
-                                            }}"
-                                            alt=""
-                                        />
+                                        <img src="{{ asset('assets/impact/3.png') }}"
+                                            alt="" />
                                     </div>
                                     <div class="impact-text">
                                         <h5>I for Iclusion</h5>
@@ -97,12 +83,8 @@
                             <a href="#">
                                 <div class="impact-card">
                                     <div class="impact-icon">
-                                        <img
-                                            src="{{
-                                                asset('assets/impact/4.png')
-                                            }}"
-                                            alt=""
-                                        />
+                                        <img src="{{ asset('assets/impact/4.png') }}"
+                                            alt="" />
                                     </div>
                                     <div class="impact-text">
                                         <h5>I for Iclusion</h5>
@@ -113,12 +95,8 @@
                             <a href="#">
                                 <div class="impact-card">
                                     <div class="impact-icon">
-                                        <img
-                                            src="{{
-                                                asset('assets/impact/5.png')
-                                            }}"
-                                            alt=""
-                                        />
+                                        <img src="{{ asset('assets/impact/5.png') }}"
+                                            alt="" />
                                     </div>
                                     <div class="impact-text">
                                         <h5>I for Iclusion</h5>
@@ -129,12 +107,8 @@
                             <a href="#">
                                 <div class="impact-card">
                                     <div class="impact-icon">
-                                        <img
-                                            src="{{
-                                                asset('assets/impact/6.png')
-                                            }}"
-                                            alt=""
-                                        />
+                                        <img src="{{ asset('assets/impact/6.png') }}"
+                                            alt="" />
                                     </div>
                                     <div class="impact-text">
                                         <h5>I for Iclusion</h5>
@@ -149,91 +123,70 @@
             <span class="total-projects">Impacts</span>
             <div class="row grid">
                 @if (count($projects) === 0)
-                <center>
-                    <p class="text-2xl text-red-600 font-bold">
-                        There are not any projects avaliable yet.
-                    </p>
-                </center>
-                @elseif(count($projects) >= 1) @foreach ($projects as $project)
-                <div
-                    class="col-lg-4 col-md-4 grid-item mental-health-portfolio"
-                >
-                    <div class="portfolio-item-wrapper">
-                        <div class="portfolio-img">
-                            <a
-                                href="{{ route('single_project', ['slug' => $project->slug]) }}"
-                                ><img
-                                    src="/storage/{{ $project->image }}"
-                                    alt="#"
-                                    class="projectImage"
-                            /></a>
-                        </div>
-                        <div class="portfolio-overlay">
-                            <div class="pf-content">
-                                {{--
-                                <span class="category">{{
-                                    $project->sub_title }}</span>
-                                --}}
-                                <h4>
-                                    <a
-                                        href="{{ route('single_project', ['slug' => $project->slug]) }}"
-                                        >{{
-                                        $project->title }}</a
-                                    >
-                                </h4>
-                                <span class="location">
-                                    <i class="lni lni-alarm-clock"></i
-                                    >{{
-                                    $project->status }}
-                                </span>
+                    <center>
+                        <p class="text-2xl text-red-600 font-bold">
+                            There are not any projects avaliable yet.
+                        </p>
+                    </center>
+                @elseif(count($projects) >= 1)
+                    @foreach ($projects as $project)
+                        <div class="col-lg-4 col-md-4 grid-item mental-health-portfolio">
+                            <div class="portfolio-item-wrapper">
+                                <div class="portfolio-img">
+                                    <a href="{{ route('single_project', ['slug' => $project->slug]) }}"><img
+                                            src="/storage/{{ $project->image }}" alt="#"
+                                            class="projectImage" /></a>
+                                </div>
+                                <div class="portfolio-overlay">
+                                    <div class="pf-content">
+                                        {{-- <span class="category">{{
+                                    $project->sub_title }}</span> --}}
+                                        <h4>
+                                            <a
+                                                href="{{ route('single_project', ['slug' => $project->slug]) }}">{{ $project->title }}</a>
+                                        </h4>
+                                        <span class="location">
+                                            <i
+                                                class="lni lni-alarm-clock"></i>{{ $project->status }}
+                                        </span>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </div>
-                @endforeach @endif @if ($past_projects) @foreach ($past_projects
-                as $project)
-                <div
-                    class="col-lg-4 col-md-4 grid-item mental-health-portfolio"
-                >
-                    <div class="portfolio-item-wrapper">
-                        <div class="portfolio-img">
-                            <a
-                                href="{{ route('single_project', ['slug' => $project->slug]) }}"
-                                ><img
-                                    src="/storage/{{ $project->image }}"
-                                    alt="#"
-                                    class="projectImage"
-                            /></a>
-                        </div>
-                        <div class="portfolio-overlay">
-                            <div class="pf-content">
-                                <span class="category">{{ $project->sub_title
-                                }}</span>
-                                <h4>
-                                    <a
-                                        href="{{ route('single_project', ['slug' => $project->slug]) }}"
-                                        >{{
-                                        $project->title }}</a
-                                    >
-                                </h4>
-                                <span class="location">
-                                    <i class="lni lni-alarm-clock"></i
-                                    >{{
-                                    $project->status }}
-                                </span>
+                    @endforeach
+                    @endif @if ($past_projects)
+                        @foreach ($past_projects as $project)
+                            <div class="col-lg-4 col-md-4 grid-item mental-health-portfolio">
+                                <div class="portfolio-item-wrapper">
+                                    <div class="portfolio-img">
+                                        <a href="{{ route('single_project', ['slug' => $project->slug]) }}"><img
+                                                src="/storage/{{ $project->image }}" alt="#"
+                                                class="projectImage" /></a>
+                                    </div>
+                                    <div class="portfolio-overlay">
+                                        <div class="pf-content">
+                                            <span
+                                                class="category">{{ $project->sub_title }}</span>
+                                            <h4>
+                                                <a
+                                                    href="{{ route('single_project', ['slug' => $project->slug]) }}">{{ $project->title }}</a>
+                                            </h4>
+                                            <span class="location">
+                                                <i
+                                                    class="lni lni-alarm-clock"></i>{{ $project->status }}
+                                            </span>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                    </div>
-                </div>
-                @endforeach @endif
+                        @endforeach
+                    @endif
             </div>
-            {{--
-            <center>
+            {{-- <center>
                 {{ $projects->links() }}
-            </center>
-            --}}
-        </div>
+            </center> --}}
+    </div>
     </section>
     <!-- End Portfolio-Section -->
-</div>
+    </div>
 @endsection
