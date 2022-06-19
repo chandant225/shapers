b96eb995cfc7d1071de5f9872665c26737a9068e
Satish Nepal <nepal.satish@hotmail.com>
2022-06-19 12:46:37 +0545

    edowment page

--- resources/views/endowment_fund.blade.php
+++ resources/views/endowment_fund.blade.php
@@ -1,9 +1,71 @@
 @extends('layouts.app')
 @section('content')
-    <section class="py-5">
-        <h2>endowment page coming soon.</h2>
-        <h2>endowment page coming soon.</h2>
-        <h2>endowment page coming soon.</h2>
-        <h2>endowment page coming soon.</h2>
+     <!-- Endowment section -->
+    <section id="team" class="team section">
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <div class="section-title align-center gray-bg">
+              
+              <h2 class="wow fadeInUp" data-wow-delay=".4s">
+                Endowment fund
+              </h2>
+             
+              <p class="wow fadeInUp" data-wow-delay=".6s">
+                Global Shapers Kathmandu Hub Endowment Fund was launched to empower Nepali youth to successfully scale up projects that deliver change in our community. The grant based fund is utilized based on Hub Charter Financial procedures and we maintain full transparency by providing funding partners with quarterly and annual action reports.
+              </p>
+            </div>
+          </div>
+        </div>
+        <h4 class="my-2">Fund Contributors:</h4>
+        <div class="row">
+          <div class="col-lg-3 col-md-6 col-12">
+            <div class="single-team wow fadeInUp" data-wow-delay=".2s">
+              <div class="image">
+                <img src="assets/images/team/team1.png" alt="#" />
+              </div>
+              <div class="info-head">
+                <div class="info-box">
+                  <h4 class="name">
+                    Shivanth Pande / NIBL Capital
+                  </h4>
+                  {{-- <span class="designation">Senior Manager</span> --}}
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-6 col-12">
+            <div class="single-team wow fadeInUp" data-wow-delay=".4s">
+              <div class="image">
+                <img src="assets/images/team/team2.png" alt="#" />
+              </div>
+              <div class="info-head">
+                <div class="info-box">
+                  <h4 class="name">
+                    Lokesh Todi
+                  </h4>
+                  {{-- <span class="designation">Markeitng</span> --}}
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-6 col-12">
+            <div class="single-team wow fadeInUp" data-wow-delay=".6s">
+              <div class="image">
+                <img src="assets/images/team/team3.png" alt="#" />
+              </div>
+              <div class="info-head">
+                <div class="info-box">
+                  <h4 class="name">
+                   Sneh Rajbhandari
+                  </h4>
+                  {{-- <span class="designation">Web Developer</span> --}}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
+    <!-- End Endowment section -->
 @endsection
