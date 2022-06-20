c99170e018f5199bf55c539d69fc7eaa7b24d45f
Satish Nepal <nepal.satish@hotmail.com>
2022-06-20 17:19:55 +0545

    css fixes , anchor target blanc

--- resources/views/endowment_fund.blade.php
+++ resources/views/endowment_fund.blade.php
@@ -17,7 +17,17 @@
             </div>
           </div>
         </div>
-        <h4 class="my-2">Fund Contributors:</h4>
+        <div class="title_buttons d-flex flex-column flex-md-row justify-content-between">
+          <h4 class="mt-5 mt-md-2 mb-2 order-2 order-md-1">Fund Contributors:</h4>
+          <div class="endowment_buttons button d-flex gap-2 mt-3 mt-md-0 order-md-1">
+              <a href="mailto:globalshapersktm@gmail.com" class="btn">
+                Become a Partner
+              </a>
+              <a href="mailto:globalshapersktm@gmail.com" class="btn">
+                Become a Contributor
+              </a>
+            </div>
+        </div>
         <div class="row">
           <div class="col-lg-3 col-md-6 col-12">
             <div class="single-team wow fadeInUp" data-wow-delay=".2s">
