d7c8d2d0b6243dd49cb3fa814552b9f607c9bd60
devchandant225 <devchandant225@gmail.com>
2022-06-20 16:13:33 +0545

    blog-image

--- resources/views/components/blogs_component.blade.php
+++ resources/views/components/blogs_component.blade.php
@@ -13,9 +13,8 @@
                 @foreach ($news as $new)
                     <div class="col-lg-4 col-md-6 col-sm-12">
                         <div class="rounded shadow blog-container">
-                            <a href="/blogs/{{ $new->slug }}"><img
-                                    src="https://images.unsplash.com/photo-1549923746-c502d488b3ea?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8ZW1wbG95ZWV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
-                                    alt="blog image" class="blog-image" /></a>
+                            <a href="/blogs/{{ $new->slug }}"><img src="/storage/{{ $new->image }}" alt="blog image"
+                                    class="blog-image" /></a>
                             <a href="/blogs/{{ $new->slug }}">
                                 <h5 class="py-3 px-3 mt-2">{{ $new->title }}</h5>
                             </a>
